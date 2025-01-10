fa64fc5b62cbaffb16023f9c3152ae3fb9a61276
Валерій Зінченко <valeriyzinchenko@Valerijs-MacBook-Pro.local>
2025-01-10 22:00:41 +0100

    fixed vercel check

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -7,10 +7,10 @@ export function CardContent(props: React.PropsWithChildren<{ className?: string 
     return <div className={`p-4 ${props.className}`}>{props.children}</div>;
 }
 
-export function CardHeader(props: React.PropsWithChildren<{}>) {
-    return <div className="p-4 border-b">{props.children}</div>;
+export function CardHeader(props: React.PropsWithChildren<{ className?: string }>) {
+    return <div className={`p-4 border-b ${props.className}`}>{props.children}</div>;
 }
 
-export function CardTitle(props: React.PropsWithChildren<{}>) {
-    return <h2 className="text-xl font-semibold">{props.children}</h2>;
+export function CardTitle(props: React.PropsWithChildren<{ className?: string }>) {
+    return <h2 className={`text-xl font-semibold ${props.className}`}>{props.children}</h2>;
 }
